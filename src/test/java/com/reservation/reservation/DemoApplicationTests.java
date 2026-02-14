91740707acf3f850cf637d4cda97601294d88414
HyeonSeo <168178115+khseo05@users.noreply.github.com>
2026-02-14 04:25:00 +0000

    init spring boot project

---
+++ src/test/java/com/reservation/reservation/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
