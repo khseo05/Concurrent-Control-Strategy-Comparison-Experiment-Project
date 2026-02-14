d820b0685b9fa80bed8af742e00a17e1de088183
HyeonSeo <168178115+khseo05@users.noreply.github.com>
2026-02-14 08:13:17 +0000

    Step1: 기본 예약 시스템 구현 및 동시성 테스트 초기 버전

---
+++ src/main/java/com/reservation/reservation/service/ConcertService.java
@@ -0,0 +1,27 @@
+package com.reservation.reservation.service;
+
+import com.reservation.reservation.domain.Concert;
+import com.reservation.reservation.repository.ConcertRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ConcertService {
+
+    private final ConcertRepository concertRepository;
+
+    @Transactional
+    public void reserse(Long concertId) {
+        Concert concert = concertRepository.findById(concertId).orElseThrow();
+
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        
+        concert.decreaseSeat();
+    }
+}
