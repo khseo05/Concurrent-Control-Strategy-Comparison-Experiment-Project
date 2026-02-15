c15b25ba0720d58bebc3a2234e3665da21cb31cd
HyeonSeo <168178115+khseo05@users.noreply.github.com>
2026-02-15 10:06:16 +0000

    feat: optimistic lock 충돌 재현 및 동시성 실험 완료

--- src/main/java/com/reservation/reservation/service/ConcertService.java
+++ src/main/java/com/reservation/reservation/service/ConcertService.java
@@ -13,30 +13,22 @@ public class ConcertService {
 
     private final ConcertRepository concertRepository;
 
+    @Transactional
     public void reserve(Long concertId) {
         int maxRetry = 10;
 
         for (int i = 0; i < maxRetry; i++) {
             try {
-                reserveOnce(concertId);
+                Concert concert = concertRepository.findById(concertId).orElseThrow();
+                Thread.sleep(1000);
+                concert.decreaseSeat();
                 return;
             } catch (ObjectOptimisticLockingFailureException e) {
-                // 충돌 -> 재시도
+                System.out.println("충돌 발생 - 재시도");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-            throw new IllegalStateException("충돌 과다 발생");
         }
-    }
-
-    @Transactional
-    public void reserveOnce(Long concertId) {
-        Concert concert = concertRepository.findById(concertId).orElseThrow();
-
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-
-        concert.decreaseSeat();
+        throw new IllegalStateException("충돌 과다 발생");
     }
 }
